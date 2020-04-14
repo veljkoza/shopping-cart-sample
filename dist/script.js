3c0bc84b1204fc6df05ca40c5ca8871f77081e3d
veljkoza <veljko.sekulic1998@gmail.com>
2020-04-14 23:22:15 +0200

    modal window integrated

--- dist/script.js
+++ dist/script.js
@@ -10,8 +10,27 @@ const addBtn = $("#add-btn");
 
 const listProducts = $(".list-products");
 
+const modalContainer = $(".modal-container");
+const modal = $(".modal");
+const hideModalBtn = $("#hideModal");
+
 if (localStorage.getItem("allItemsJsonId") === null) {
   createItemsJson();
+} else {
+  loadItems();
+}
+
+async function loadItems() {
+  let blobId = localStorage.getItem("allItemsJsonId");
+  let items = [];
+  await fetch(`https://jsonblob.com/api/jsonBlob/${blobId}`, {
+    method: "GET",
+  }).then(res=>res.json())
+  .then(data => console.log(data));
+}
+
+function loadItem(itemObj){
+  
 }
 
 async function createItemsJson() {
@@ -47,15 +66,23 @@ function addNewItem(e) {
 
   let id = Math.random().toString(16).slice(2);
   newProductDiv.classList.add("product");
+  newProductDiv.setAttribute("data-id", id);
   newProductDiv.innerHTML = ` 
     <img
         src="${img}"
     />
     <p>${name}</p>
-    <p>$${price}</p>
-    <button class="details-button">Details</button>
-    <button class="buy-button">Buy</button>`;
+    <p>$${price}</p>`;
+  let detailsBtn = document.createElement("button");
+  detailsBtn.classList.add("details-button");
+  detailsBtn.textContent = "Details";
+  let buyBtn = document.createElement("button");
+  buyBtn.classList.add("buy-button");
+  buyBtn.textContent = "Buy";
 
+  detailsBtn.addEventListener("click", showModalWindow);
+  newProductDiv.appendChild(detailsBtn);
+  newProductDiv.appendChild(buyBtn);
   listProducts.appendChild(newProductDiv);
 
   let newItem = {};
@@ -66,6 +93,56 @@ function addNewItem(e) {
   newItem.price = price;
 
   addNewItemToJson(newItem);
+}
+
+async function showModalWindow(e) {
+  let itemId = this.parentElement.getAttribute("data-id");
+  let item;
+  await getItemById(itemId)
+    .then((res) => res)
+    .then((data) => (item = data));
+
+  modal.innerHTML = "";
+  let modalLeft = document.createElement("div");
+  modalLeft.classList.add("modal-left");
+  modalLeft.style.backgroundImage = `url('${item.img}')`;
+  console.log(modalLeft);
+  modal.appendChild(modalLeft);
+
+  let modalRight = document.createElement("div");
+  modalRight.classList.add("modal-right");
+  modalRight.innerHTML = `<button id="hideModal"><i class="fas fa-times"></i></button>
+  <p>${item.name}</p>
+  <p>
+  ${item.desc}
+  </p>
+  <span></span>
+  <div class="modal-bot">
+    <button class="buy-button">Buy</button>
+    <p>$${item.price}</p>
+  </div>`;
+  let hideModalBtn = modalRight.firstElementChild;
+  console.log(hideModalBtn);
+  hideModalBtn.addEventListener("click", (e) => {
+    modalContainer.classList.remove("active");
+  });
+  modal.appendChild(modalRight);
+  modalContainer.classList.add("active");
+}
+
+async function getItemById(id) {
+  let blobId = localStorage.getItem("allItemsJsonId");
+  let item = {};
+  console.log("test");
+  await fetch(`https://jsonblob.com/api/jsonBlob/${blobId}`, {
+    method: "GET",
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      item = data.items.find((item) => item.id === id);
+    });
+
+  return item;
 }
 
 async function addNewItemToJson(newItem) {
